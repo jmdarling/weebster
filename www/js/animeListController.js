76d05722a580a4d2f3cb9d3f1c2d17015ddc801b
Jonathan Darling <jmdarling@me.com>
2015-12-28 18:06:18 -0600

    Fixes URL for library endpoint

--- www/js/animeListController.js
+++ www/js/animeListController.js
@@ -1,6 +1,6 @@
 (function() {
   function animeListController($scope, $http) {
-    $http.jsonp('https://hummingbird.me/api/v1/users/coltex/library')
+    $http.get('http://localhost:3000/users/coltex/library')
       .then(function(response) {
         $scope.libraryEntries = response.data;
       })
