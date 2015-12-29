84e2679af20e02b76fe4bf0ec666a37244cfd8ca
Jonathan Darling <jmdarling@me.com>
2015-12-29 12:12:07 -0600

    Adds refresh capability.

--- www/js/animeListController.js
+++ www/js/animeListController.js
@@ -7,6 +7,16 @@
       .catch(function(error) {
         console.log(error);
       });
+
+      $scope.onRefresh = function () {
+        $http.get('https://weebster-server.herokuapp.com/users/coltex/library')
+          .then(function(response) {
+            $scope.libraryEntries = response.data;
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
   }
 
   angular.module('weebster').controller('animeListController', ['$scope', '$http', animeListController]);
