9f08862808b648d2056fc59113c2e2da2208d8f2
Jonathan Darling <jmdarling@me.com>
2016-01-01 17:33:20 -0600

    Adds filter for library status.

--- www/js/controllers/animeListController.js
+++ www/js/controllers/animeListController.js
@@ -1,7 +1,47 @@
 /* globals angular */
 (function () {
   function animeListController ($scope, $http, $state, sessionService) {
-    $http.get('https://weebster-server.herokuapp.com/users/' + sessionService.getUsername() + '/library')
+    $scope.libraryStateOptions = [
+      {
+        id: 'all',
+        name: 'All'
+      },
+      {
+        id: 'currently-watching',
+        name: 'Currently Watching'
+      },
+      {
+        id: 'plan-to-watch',
+        name: 'Plan to Watch'
+      },
+      {
+        id: 'completed',
+        name: 'Completed'
+      },
+      {
+        id: 'on-hold',
+        name: 'On Hold'
+      },
+      {
+        id: 'dropped',
+        name: 'Dropped'
+      }
+    ]
+
+    $scope.selected = {}
+    $scope.selected.libraryState = $scope.libraryStateOptions[1]
+
+    $scope.onSelectedLibraryStateChanged = function () {
+      $http.get('https://weebster-server.herokuapp.com/users/' + sessionService.getUsername() + '/library?status=' + $scope.selected.libraryState.id)
+      .then(function (response) {
+        $scope.libraryEntries = response.data
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
+
+    $http.get('https://weebster-server.herokuapp.com/users/' + sessionService.getUsername() + '/library?status=' + $scope.selected.libraryState.id)
       .then(function (response) {
         $scope.libraryEntries = response.data
       })
