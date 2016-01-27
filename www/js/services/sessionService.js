bd981c129b89bc6b1316a6c403751ad6e281d824
Jonathan Darling <jmdarling@me.com>
2016-01-26 21:10:55 -0600

    Documets the session service

--- www/js/services/sessionService.js
+++ www/js/services/sessionService.js
@@ -4,6 +4,11 @@
     var authenticationTokenKey = 'auth'
     var usernameKey = 'username'
 
+    /**
+     * Returns whether or not the user has a session.
+     *
+     * @return {Boolean} True if the user has a session, false otherwise.
+     */
     this.hasSession = function () {
       var hasAuthenticationToken = (window.localStorage.getItem(authenticationTokenKey) != null)
       var hasUsername = (window.localStorage.getItem(usernameKey) != null)
@@ -11,19 +16,38 @@
       return hasAuthenticationToken && hasUsername
     }
 
+    /**
+     * Starts the user's session.
+     *
+     * @param  {string} authenticationToken The user's authentication token.
+     * @param  {string} username            The user's username.
+     */
     this.startSession = function (authenticationToken, username) {
       window.localStorage.setItem(authenticationTokenKey, authenticationToken)
       window.localStorage.setItem(usernameKey, username)
     }
 
+    /**
+     * Ends the user's session.
+     */
     this.endSession = function () {
       window.localStorage.removeItem(authenticationTokenKey)
     }
 
+    /**
+     * Returns the user's authentication token.
+     *
+     * @return {string} The user's authentication token.
+     */
     this.getAuthenticationToken = function () {
       return window.localStorage.getItem(authenticationTokenKey)
     }
 
+    /**
+     * Returns the user's username.
+     *
+     * @return {string} The user's username.
+     */
     this.getUsername = function () {
       return window.localStorage.getItem(usernameKey)
     }
