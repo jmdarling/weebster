04ab1d7f97327d2e0964bd4d4aef875775415a9f
Jonathan Darling <jmdarling@me.com>
2016-02-08 21:52:51 -0600

    Finished duplicating functionality from old edit modal.

--- es6/controllers/animeDetailController.js
+++ es6/controllers/animeDetailController.js
@@ -1,10 +1,22 @@
 /* globals angular */
 (() => {
-  angular.module('weebster').controller('animeDetailController', ['$scope', '$stateParams', '$ionicPopup', 'dataService', 'listItemUpdateService', ($scope, $stateParams, $ionicPopup, dataService, listItemUpdateService) => {
+  angular.module('weebster').controller('animeDetailController', ['$scope', '$stateParams', '$ionicPopup', '$ionicLoading', 'dataService', 'listItemUpdateService', ($scope, $stateParams, $ionicPopup, $ionicLoading, dataService, listItemUpdateService) => {
     // Initialization //
     $scope.libraryEntry = $stateParams.libraryEntry
 
+    function showLoadingIndicator () {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>'
+      })
+    }
+
+    function hideLoadingIndicator () {
+      $ionicLoading.hide()
+    }
+
     $scope.incrementWatchCount = () => {
+      showLoadingIndicator()
+
       dataService.incrementEpisodesWatched($scope.libraryEntry.anime.id)
         .then((response) => {
           listItemUpdateService.update($scope.libraryEntry)
@@ -16,6 +28,33 @@
             template: 'There was a problem updating this anime, please try again later.'
           })
         })
+        .finally(hideLoadingIndicator)
+    }
+
+    $scope.buildRatingStarsArray = rating => {
+      let starsArray = []
+
+      let fullStars = Math.floor(rating)
+      let hasHalfStar = (rating % 1) > 0.25
+
+      let iteration = 0
+
+      while (iteration < fullStars) {
+        starsArray[iteration] = 1
+        iteration += 1
+      }
+
+      if (hasHalfStar) {
+        starsArray[iteration] = 0.5
+        iteration += 1
+      }
+
+      while (iteration < 5) {
+        starsArray[iteration] = 0
+        iteration += 1
+      }
+
+      return starsArray
     }
   }])
 })()
--- www/js/controllers/animeDetailController.js
+++ www/js/controllers/animeDetailController.js
@@ -2,11 +2,23 @@
 
 /* globals angular */
 (function () {
-  angular.module('weebster').controller('animeDetailController', ['$scope', '$stateParams', '$ionicPopup', 'dataService', 'listItemUpdateService', function ($scope, $stateParams, $ionicPopup, dataService, listItemUpdateService) {
+  angular.module('weebster').controller('animeDetailController', ['$scope', '$stateParams', '$ionicPopup', '$ionicLoading', 'dataService', 'listItemUpdateService', function ($scope, $stateParams, $ionicPopup, $ionicLoading, dataService, listItemUpdateService) {
     // Initialization //
     $scope.libraryEntry = $stateParams.libraryEntry;
 
+    function showLoadingIndicator() {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>'
+      });
+    }
+
+    function hideLoadingIndicator() {
+      $ionicLoading.hide();
+    }
+
     $scope.incrementWatchCount = function () {
+      showLoadingIndicator();
+
       dataService.incrementEpisodesWatched($scope.libraryEntry.anime.id).then(function (response) {
         listItemUpdateService.update($scope.libraryEntry);
         $scope.libraryEntry.episodes_watched += 1;
@@ -15,7 +27,33 @@
           title: 'Update Error',
           template: 'There was a problem updating this anime, please try again later.'
         });
-      });
+      }).finally(hideLoadingIndicator);
+    };
+
+    $scope.buildRatingStarsArray = function (rating) {
+      var starsArray = [];
+
+      var fullStars = Math.floor(rating);
+      var hasHalfStar = rating % 1 > 0.25;
+
+      var iteration = 0;
+
+      while (iteration < fullStars) {
+        starsArray[iteration] = 1;
+        iteration += 1;
+      }
+
+      if (hasHalfStar) {
+        starsArray[iteration] = 0.5;
+        iteration += 1;
+      }
+
+      while (iteration < 5) {
+        starsArray[iteration] = 0;
+        iteration += 1;
+      }
+
+      return starsArray;
     };
   }]);
 })();
